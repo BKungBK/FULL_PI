c3a627b06d6acbd3444d815a3ecc4d8300e0f1aa
BKungBK <bkungbk12@gmail.com>
2026-04-13 12:19:01 +0700

    FULL TEST6

--- CameraWebServer1.3.69/CameraWebServer1.3.69.ino
+++ CameraWebServer1.3.69/CameraWebServer1.3.69.ino
@@ -110,7 +110,7 @@ void setup() {
   // initial sensors are flipped vertically and colors are a bit saturated
   if (s->id.PID == OV3660_PID) {
     s->set_vflip(s, 0); // flip it back
-    s->set_brightness(s, 1); // up the brightness just a bit
+    s->set_brightness(s, -1); // up the brightness just a bit
     s->set_saturation(s, -2); // lower the saturation
   }
 
